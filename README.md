318aa17d1e1cf73c04aa566cda27cc74049aab18
frankdb <fd.barros@yahoo.com>
2019-03-10 10:01:25 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project was built for San Diego Code School. It is a simple http server built with Node.
+
+To view the website, open up the terminal, navigate to the directory and type "npm start".
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code here
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  console.log(`Received ${request.method} request for: ${request.url}`);
+  response.end("<h1>Hello World</h1>");
+});
+
+server.listen(8080, () => {
+  console.log("Server is listening on localhost:8080");
+});
